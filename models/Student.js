0240788ae732a8e82877c6355a95a89a21d62e19
Princy Sharma <princysharma09@gmail.com>
2025-09-04 12:24:25 +0530

    edit mother field

--- models/Student.js
+++ models/Student.js
@@ -6,6 +6,7 @@ const studentSchema = new Schema({
     rollNo : {type : Number, required : true},
     name : {type: String , required : true},
     fatherName : {type: String ,required : true },
+    motherName : {type: String },
     course: {type: String , required : true},
     branch : {type: String , required : true},
     yearOfAdmission : {type : String},
